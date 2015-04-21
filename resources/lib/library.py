6f34a7e8aab1796d39930055117ecb43d02c768f
HitcherUK <HitcherUK@gmail.com>
2015-04-21 16:54:49 +0100

    Add shutdown timer functions

--- resources/lib/library.py
+++ resources/lib/library.py
@@ -712,6 +712,8 @@ class LibraryFunctions():
             listitems.append( self._create(["Quit", "13009", "32054", {} ]) )
             listitems.append( self._create(["Hibernate", "13010", "32054", {} ]) )
             listitems.append( self._create(["Suspend", "13011", "32054", {} ]) )
+            listitems.append( self._create(["AlarmClock(shutdowntimer,XBMC.Shutdown())", "19026", "32054", {} ]) )
+            listitems.append( self._create(["CancelAlarm(shutdowntimer)", "20151", "32054", {} ]) )
             if xbmc.getCondVisibility( "System.HasLoginScreen" ):
                 listitems.append( self._create(["System.LogOff", "20126", "32054", {} ]) )
             listitems.append( self._create(["ActivateScreensaver", "360", "32054", {} ]) )
